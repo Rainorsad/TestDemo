2ea6a28c3ff8437afd81c013a79883e6945f1f2d
Rainorsad <457072858@qq.com>
2017-05-25 15:53:23 +0800

    Signed-off-by: Rainorsad <457072858@qq.com>

--- app/src/main/java/com/example/wb/testdemo/guaguale/GuaguaLeActivity.java
+++ app/src/main/java/com/example/wb/testdemo/guaguale/GuaguaLeActivity.java
@@ -6,15 +6,23 @@ import android.support.v7.app.AppCompatActivity;
 
 import com.example.wb.testdemo.R;
 
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+
 /**
  * Created by Zhangchen on 2017/5/24.
  */
 
-public class GuaguaLeActivity extends AppCompatActivity{
+public class GuaguaLeActivity extends AppCompatActivity {
+
+
+    @InjectView(R.id.image)
+    GuaguaLeView image;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_guaguale);
+        ButterKnife.inject(this);
     }
 }
