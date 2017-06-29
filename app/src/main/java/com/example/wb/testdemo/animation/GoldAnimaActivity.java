64e8c4539bc5f4e9da36bfff2ae1bcd5d21fc048
Rainorsad <457072858@qq.com>
2017-06-29 17:28:32 +0800

    Signed-off-by: Rainorsad <457072858@qq.com>

--- app/src/main/java/com/example/wb/testdemo/animation/GoldAnimaActivity.java
+++ app/src/main/java/com/example/wb/testdemo/animation/GoldAnimaActivity.java
@@ -3,18 +3,37 @@ package com.example.wb.testdemo.animation;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.example.wb.testdemo.R;
+
+import butterknife.ButterKnife;
+import butterknife.InjectView;
+import butterknife.OnClick;
 
 /**
  * Created by Zhangchen on 2017/6/29.
  */
 
-public class GoldAnimaActivity extends AppCompatActivity{
+public class GoldAnimaActivity extends AppCompatActivity {
+
+    @InjectView(R.id.button)
+    Button button;
+    @InjectView(R.id.textview)
+    TextView textview;
+    @InjectView(R.id.ll)
+    LinearLayout ll;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_goldanimal);
+        ButterKnife.inject(this);
+    }
+
+    @OnClick(R.id.button)
+    public void onClick() {
     }
 }
