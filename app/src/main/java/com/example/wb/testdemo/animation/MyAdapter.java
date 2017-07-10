17a10eefb86b04d3a7050badaef84e012e9cd69e
Rainorsad <457072858@qq.com>
2017-07-10 17:30:39 +0800

    Signed-off-by: Rainorsad <457072858@qq.com>

---
+++ app/src/main/java/com/example/wb/testdemo/animation/MyAdapter.java
@@ -0,0 +1,117 @@
+package com.example.wb.testdemo.animation;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.example.wb.testdemo.R;
+
+/**
+ * Created by Zhangchen on 2017/7/10.
+ */
+
+public class MyAdapter extends RecyclerView.Adapter {
+    public static final int TYPE_HEADER = 0;  //说明是带有Header的
+    public static final int TYPE_FOOTER = 1;  //说明是带有Footer的
+    public static final int TYPE_NORMAL = 2;  //说明是不带有header和footer的
+
+    private View mHeaderView;
+    private View mFooterView;
+
+    public View getmFooterView() {
+        return mFooterView;
+    }
+
+    public void setmFooterView(View mFooterView) {
+        this.mFooterView = mFooterView;
+        notifyItemInserted(getItemCount() - 1);
+    }
+
+    public View getmHeaderView() {
+        return mHeaderView;
+    }
+
+    public void setmHeaderView(View mHeaderView) {
+        this.mHeaderView = mHeaderView;
+        notifyItemInserted(0);
+    }
+
+    private String[] data;
+    private Context context;
+
+    public MyAdapter(Context context, String[] list) {
+        this.context = context;
+        this.data = list;
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        if (mHeaderView == null && mFooterView == null) {
+            return TYPE_NORMAL;
+        }
+        if (position == 0) {
+            return TYPE_HEADER;
+        }
+        if (position == getItemCount() - 1) {
+            return TYPE_FOOTER;
+        }
+        return TYPE_NORMAL;
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        if (mHeaderView != null && viewType == TYPE_HEADER) {
+            return new MyViewHolder(mHeaderView);
+        }
+        if (mFooterView != null && viewType == TYPE_FOOTER) {
+            return new MyViewHolder(mFooterView);
+        }
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_textviewcolor, parent, false);
+        RecyclerView.ViewHolder holder = new MyViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        if (getItemViewType(position) == TYPE_NORMAL) {
+            MyViewHolder myViewHolder = (MyViewHolder) holder;
+            myViewHolder.tv.setText(data[position]);
+            return;
+        }else if (getItemViewType(position) == TYPE_HEADER){
+            return;
+        }else {
+            return;
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        if (mHeaderView == null && mFooterView == null) {
+            return data.length;
+        }else if(mHeaderView == null && mFooterView != null){
+            return data.length + 1;
+        }else if (mHeaderView != null && mFooterView == null){
+            return data.length + 1;
+        }else {
+            return data.length + 2;
+        }
+    }
+
+    class MyViewHolder extends RecyclerView.ViewHolder {
+        TextView tv;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            if (itemView == mHeaderView){
+                return;
+            }
+            if (itemView == mFooterView){
+                return;
+            }
+            tv = (TextView) itemView.findViewById(R.id.tv);
+        }
+    }
+}
--- app/src/main/java/com/example/wb/testdemo/itemDecoration/ItemOritionActivity.java
+++ app/src/main/java/com/example/wb/testdemo/itemDecoration/ItemOritionActivity.java
@@ -7,10 +7,10 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.wb.testdemo.R;
+import com.example.wb.testdemo.animation.MyAdapter;
 
 import butterknife.ButterKnife;
 import butterknife.InjectView;
@@ -38,36 +38,15 @@ public class ItemOritionActivity extends AppCompatActivity {
         orition.setHeight(2);
         recycleview.addItemDecoration(orition);
         recycleview.setLayoutManager(manager);
-        recycleview.setAdapter(new MyAdapter());
+
+        MyAdapter adapter = new MyAdapter(this,data);
+        recycleview.setAdapter(adapter);
+        
+        View headview = LayoutInflater.from(ItemOritionActivity.this).inflate(R.layout.activity_textviewcolor,recycleview,false);
+        TextView tvhead = (TextView) headview.findViewById(R.id.tv);
+        tvhead.setText("我是头部");
+        adapter.setmHeaderView(tvhead);
+
     }
 
-    class  MyAdapter extends RecyclerView.Adapter{
-
-        @Override
-        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_textviewcolor,parent,false);
-            RecyclerView.ViewHolder holder = new MyViewHolder(view);
-            return holder;
-        }
-
-        @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-            MyViewHolder myViewHolder = (MyViewHolder) holder;
-            myViewHolder.tv.setText(data[position]);
-        }
-
-        @Override
-        public int getItemCount() {
-            return data.length;
-        }
-
-        class MyViewHolder extends RecyclerView.ViewHolder{
-
-            private TextView tv;
-            public MyViewHolder(View itemView) {
-                super(itemView);
-                tv = (TextView) itemView.findViewById(R.id.tv);
-            }
-        }
-    }
 }
